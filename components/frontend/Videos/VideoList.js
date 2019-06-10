4da3e3e7cd6533d261ddd3d1dba5a28e5ff87451
Faheem Nabi <47860992+faheemnabi913@users.noreply.github.com>
2019-06-10 19:04:03 +0530

    Add files via upload
    10th june only that working issue

--- components/frontend/Videos/VideoList.js
+++ components/frontend/Videos/VideoList.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { FlatList, ActivityIndicator, View  } from 'react-native';
-import VideoDetail from './VideoDetail';
+import { FlatList, ActivityIndicator, View, TouchableOpacity, StyleSheet,Image, Text } from 'react-native';
+import VideoDetail from './VideoDetail'; //no need if this works
+import VideoCard from './VideoCard';
+import CardSection from './CardSection';
 
-var region = "GB"; // others are IN,PK,US,AU,CA
+var region = "GB"; // others are IN,PK,US,AU,CAcan be utilised with dropdown and state property
 var key = "AIzaSyCQXmAB1Nc5ZI2C770jdmqY0tEYidUmauo"
 var url = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=10&regionCode=${region}&key=${key}`;
 
@@ -11,6 +13,9 @@ export default class VideoList extends React.Component {
   constructor(props){
     super(props);
     this.state ={ isLoading: true}
+  }
+  static navigationOptions = {
+    title: 'Videos'
   }
 
   componentDidMount(){
@@ -47,10 +52,49 @@ export default class VideoList extends React.Component {
       <View style={{flex: 1}}>
         <FlatList
           data={this.state.dataSource}
-          renderItem={({item}) => <VideoDetail video={item}/>}
+          renderItem={({item}) => <TouchableOpacity 
+          key={item.id}
+          onPress={() => this.props.navigation.navigate('PlayVideo', {youtubeId : key})}
+          >
+              <VideoCard>
+                  <CardSection>
+                      <View style={styles.viewStyle}>
+                          <Image
+                              style = {styles.imageStyle}
+                              source={{uri: item.snippet.thumbnails.default.url}}
+                          />
+                      </View>
+                      <View style={styles.contentStyle}>
+                          <Text>{item.snippet.title}</Text>
+                          <Text>{item.snippet.channelTitle}</Text>
+                      </View>
+
+                  </CardSection>
+                  
+              
+              </VideoCard>
+          </TouchableOpacity>}
           keyExtractor={({id}) => id}
         />
       </View>
     );
   }
 }
+
+ const styles = StyleSheet.create({
+     contentStyle :{
+         justifyContent : 'space-around',
+         flexShrink : 1,
+         flexDirection :'column',
+         paddingLeft : 15,
+         
+     },
+     imageStyle : {
+         width : 100,
+         height :100,
+         justifyContent:'center'
+     },
+     viewStyle:{
+         justifyContent:'center'
+     }
+ })
