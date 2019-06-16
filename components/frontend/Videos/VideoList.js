71c1545fc1fc864bf87a134dda8ab6fcda639e43
Faheem Nabi <47860992+faheemnabi913@users.noreply.github.com>
2019-06-16 23:27:30 +0530

    Add files via upload
    image not displayed

--- components/frontend/Videos/VideoList.js
+++ components/frontend/Videos/VideoList.js
@@ -1,18 +1,45 @@
 import React from 'react';
-import { FlatList, ActivityIndicator, View, TouchableOpacity, StyleSheet,Image, Text } from 'react-native';
-import VideoDetail from './VideoDetail'; //no need if this works
-import VideoCard from './VideoCard';
-import CardSection from './CardSection';
+import { FlatList, View, TouchableOpacity, StyleSheet,Image } from 'react-native';
+import { Container, Content, Card, CardItem, Text, Button, Icon, Left, Body, Right, Spinner } from 'native-base'
 
-var region = "GB"; // others are IN,PK,US,AU,CAcan be utilised with dropdown and state property
+var region = "IN"; // others are IN,PK,US,AU,CAcan be utilised with dropdown and state property
 var key = "AIzaSyCQXmAB1Nc5ZI2C770jdmqY0tEYidUmauo"
 var url = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=10&regionCode=${region}&key=${key}`;
+
+// const region = [
+//   {
+//     value: 'IN',
+//     label: 'IN'
+//   },
+//   {
+//     value: 'PK',
+//     label: 'PK'
+//   },
+//   {
+//     value: 'US',
+//     label: 'US'
+//   },
+//   {
+//     value: 'GB',
+//     label: 'UK'
+//   },
+//   {
+//     value: 'AU',
+//     label: 'AUS'
+//   },
+//   {
+//     value: 'CA',
+//     label: 'CAN'
+//   }
+// ]
 
 export default class VideoList extends React.Component {
 
   constructor(props){
     super(props);
-    this.state ={ isLoading: true}
+    this.state = {
+      isLoading: true,
+      }
   }
   static navigationOptions = {
     title: 'Videos'
@@ -36,6 +63,19 @@ export default class VideoList extends React.Component {
       });
   }
 
+   nFormatter = (num) => {
+     if (num >= 1000000000) {
+        return (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'G';
+     }
+     if (num >= 1000000) {
+        return (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+     }
+     if (num >= 1000) {
+        return (num / 1000).toFixed(1).replace(/\.0$/, '') + 'K';
+     }
+     return num;
+  }
+
 
 
   render(){
@@ -43,7 +83,7 @@ export default class VideoList extends React.Component {
     if(this.state.isLoading){
       return(
         <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
+          <Spinner/>
         </View>
       )
     }
@@ -54,25 +94,48 @@ export default class VideoList extends React.Component {
           data={this.state.dataSource}
           renderItem={({item}) => <TouchableOpacity 
           key={item.id}
-          onPress={() => this.props.navigation.navigate('PlayVideo', {youtubeId : key})}
+          onPress={() => this.props.navigation.navigate('PlayVideo', {youtubeId : item.id})}
           >
-              <VideoCard>
-                  <CardSection>
-                      <View style={styles.viewStyle}>
-                          <Image
-                              style = {styles.imageStyle}
-                              source={{uri: item.snippet.thumbnails.default.url}}
-                          />
-                      </View>
-                      <View style={styles.contentStyle}>
-                          <Text>{item.snippet.title}</Text>
-                          <Text>{item.snippet.channelTitle}</Text>
-                      </View>
+              <Content padder>
+                <Card>
+                    <CardItem cardBody>
+                      <Image source={{uri: item.snippet.thumbnails.medium}} style={{height: 200, width: null, flex: 1}}/>
+                    </CardItem>
 
-                  </CardSection>
-                  
+                  <CardItem>
+                    <Left>
+                        <Text>{item.snippet.title}</Text>
+                    </Left>
+                  </CardItem>
+
+                  <CardItem>
+                    <Text></Text>
+                    <Text note>{item.snippet.channelTitle}</Text>
+                  </CardItem>
               
-              </VideoCard>
+                  <CardItem>
+                    <Left>
+                      <Button transparent>
+                        <Icon active name="md-eye" />
+                        <Text>{this.nFormatter(item.statistics.viewCount)}</Text>
+                      </Button>
+                    </Left>
+                    <Body>
+                      <Button transparent>
+                        <Icon active name="thumbs-up" />
+                        <Text>{this.nFormatter(item.statistics.likeCount)}</Text>
+                      </Button>
+                    </Body>
+                    <Right>
+                      <Button transparent>
+                        <Icon active name="thumbs-down" />
+                        <Text>{this.nFormatter(item.statistics.dislikeCount)}</Text>
+                      </Button>
+                    </Right>
+                  </CardItem>
+
+                </Card>
+              </Content>
           </TouchableOpacity>}
           keyExtractor={({id}) => id}
         />
@@ -80,21 +143,3 @@ export default class VideoList extends React.Component {
     );
   }
 }
-
- const styles = StyleSheet.create({
-     contentStyle :{
-         justifyContent : 'space-around',
-         flexShrink : 1,
-         flexDirection :'column',
-         paddingLeft : 15,
-         
-     },
-     imageStyle : {
-         width : 100,
-         height :100,
-         justifyContent:'center'
-     },
-     viewStyle:{
-         justifyContent:'center'
-     }
- })
